0ecb755110aadf4e19e5a4c5ff5feb7113f9e2bc
timoj <timoj@users.noreply.github.com>
2022-04-13 11:03:39 +0200

    config classes, renamed model to module

--- enodo/model/config/worker.py
+++ enodo/model/config/worker.py
@@ -1,7 +1,3 @@
-import json
-
-from . import ConfigModel
-
 WORKER_MODE_GLOBAL = "global"
 WORKER_MODE_DEDICATED_JOB_TYPE = "dedicated_job_type"
 WORKER_MODE_DEDICATED_SERIES = "dedicated_series"
@@ -12,34 +8,46 @@ WORKER_MODES = [
 ]
 
 
-class WorkerConfigModel(ConfigModel):
-
-    __slots__ = ('mode', 'job_type', 'series')
-
-    def __init__(self, worker_mode,
+class WorkerConfigModel(dict):
+    def __init__(self, mode,
                  dedicated_job_type=None, dedicated_series_name=None):
-        if worker_mode not in WORKER_MODES:
+        if mode not in WORKER_MODES:
             raise Exception("Invalid worker mode")
 
-        if worker_mode == WORKER_MODE_DEDICATED_JOB_TYPE and \
+        if mode == WORKER_MODE_DEDICATED_JOB_TYPE and \
                 dedicated_job_type is None:
             raise Exception("Invalid worker config")
 
-        if worker_mode == WORKER_MODE_DEDICATED_SERIES and \
+        if mode == WORKER_MODE_DEDICATED_SERIES and \
                 dedicated_series_name is None:
             raise Exception("Invalid worker config")
 
-        self.mode = worker_mode
-        self.job_type = dedicated_job_type
-        self.series = dedicated_series_name
+        super(WorkerConfigModel, self).__init__({
+            "mode": mode,
+            "dedicated_job_type": dedicated_job_type,
+            'dedicated_series_name': dedicated_series_name
+        })
 
-    @classmethod
-    def from_dict(cls, data):
-        return cls(**data)
+    @property
+    def mode(self):
+        return self.get("mode")
 
-    def to_dict(self):
-        return {
-            'worker_mode': self.mode,
-            'dedicated_job_type': self.job_type,
-            'dedicated_series_name': self.series
-        }
+    @mode.setter
+    def mode(self, value):
+        self["mode"] = value
+
+    @property
+    def dedicated_job_type(self):
+        return self.get("dedicated_job_type")
+
+    @dedicated_job_type.setter
+    def dedicated_job_type(self, value):
+        self["dedicated_job_type"] = value
+
+    @property
+    def dedicated_series_name(self):
+        return self.get("dedicated_series_name")
+
+    @dedicated_series_name.setter
+    def dedicated_series_name(self, value):
+        self["dedicated_series_name"] = value
