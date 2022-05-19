bf36c5f9e997a9bd2541c2fcb3c643a7108e1503
timoj <timoj@users.noreply.github.com>
2022-05-19 10:25:07 +0200

    client logging, client reconnect

--- enodo/client.py
+++ enodo/client.py
@@ -2,12 +2,41 @@ import asyncio
 import datetime
 import os
 import uuid
-import logging
 
 import qpack
 
+from enodo.logging import client_logger as logger
 from .protocol.package import *
 from .version import __version__ as VERSION
+
+
+class EnodoStreamReaderProtocol(asyncio.StreamReaderProtocol):
+
+    def __init__(
+            self, stream_reader, client_connected_cb=None, loop=None,
+            client_lost_connection_cb=None):
+        super(EnodoStreamReaderProtocol, self).__init__(
+            stream_reader, client_connected_cb=None, loop=None)
+        self._client_lost_connection_cb = client_lost_connection_cb
+
+    def connection_lost(self, exc):
+        super(EnodoStreamReaderProtocol, self).connection_lost(exc)
+        if self._client_lost_connection_cb is not None:
+            self._client_lost_connection_cb()
+
+
+async def open_connection(host=None, port=None,
+                          limit=asyncio.streams._DEFAULT_LIMIT,
+                          client_lost_connection_cb=None):
+    loop = asyncio.get_event_loop()
+    reader = asyncio.StreamReader(limit=limit, loop=loop)
+    protocol = EnodoStreamReaderProtocol(
+        reader, loop=loop,
+        client_lost_connection_cb=client_lost_connection_cb)
+    transport, _ = await loop.create_connection(
+        lambda: protocol, host, port)
+    writer = asyncio.StreamWriter(transport, protocol, reader, loop)
+    return reader, writer
 
 
 class Client:
@@ -30,16 +59,15 @@ class Client:
                 self._id = enodo_id
 
         self._token = token
-        self._messages = {}
-
         self._last_heartbeat_send = datetime.datetime.now()
-        self._updates_on_heartbeat = []
+        self._last_heartbeat_received = datetime.datetime.now()
         self._cbs = None
         self._handshake_data_cb = None
         self._reader = None
         self._writer = None
         self._running = True
         self._connected = False
+        self._handsshaked = False
         self._read_task = None
 
     async def setup(self, cbs=None, handshake_cb=None):
@@ -66,44 +94,57 @@ class Client:
 
     async def _connect(self):
         while not self._connected:
-            logging.info("Trying to connect")
+            logger.info("Trying to connect")
             try:
-                self._reader, self._writer = await asyncio.open_connection(
+                self._reader, self._writer = await open_connection(
                     self._hostname,
-                    self._port)
+                    self._port,
+                    client_lost_connection_cb=self.connection_lost)
             except Exception as e:
-                logging.warning(f"Cannot connect, {str(e)}")
-                logging.info("Retrying in 5")
+                logger.warning(f"Cannot connect, {str(e)}")
+                logger.info("Retrying in 5")
                 await asyncio.sleep(4)
             else:
-                logging.info("Connected")
+                logger.info("Connected")
                 self._connected = True
                 await self._handshake()
 
+    def connection_lost(self):
+        logger.warning('Connection lost')
+        self._connected = False
+        self._writer.close()
+        self._writer = None
+        self._read_task.cancel()
+
     async def run(self):
         while self._running:
-            if self._writer is not None and (
-                    self._writer.transport.
-                    _conn_lost or self._writer.transport.is_closing()):
-                logging.info('Connection lost')
-                self._connected = False
-                self._writer.close()
-                self._writer = None
-                self._read_task.cancel()
             if not self._connected:
+                if self._handsshaked:
+                    self._handsshaked = False
+                if self._writer and not self._writer.is_closing():
+                    self._writer.close()
+                    await self._writer.wait_closed()
                 await self._connect()
-            else:
+            elif self._handsshaked:
                 diff = datetime.datetime.now() - self._last_heartbeat_send
                 if diff.total_seconds() > int(
                         self._heartbeat_interval):
                     await self._send_heartbeat()
+                diff = datetime.datetime.now() - self._last_heartbeat_received
+                if diff.total_seconds() > int(
+                        2*self._heartbeat_interval):
+                    logger.error(
+                        "Haven't received heartback response from hub")
+                    self._connected = False
             await asyncio.sleep(1)
 
     async def close(self):
-        logging.info('Closing the socket')
+        logger.info('Closing the socket')
         self._running = False
-        self._read_task.cancel()
-        self._writer.close()
+        if self._read_task:
+            self._read_task.cancel()
+        if self._writer:
+            self._writer.close()
 
     async def _read_from_socket(self):
         while self._running:
@@ -120,37 +161,40 @@ class Client:
                 data = qpack.unpackb(data, decode='utf-8')
 
             if packet_type == 0:
-                logging.warning("Connection lost, trying to reconnect")
+                logger.warning("Connection lost, trying to reconnect")
                 self._connected = False
                 try:
                     await self.setup(self._cbs)
                 except Exception as e:
-                    logging.error('Error while trying to setup client')
-                    logging.debug(f'Correspondig error: {str(e)}')
+                    logger.error('Error while trying to setup client')
+                    logger.debug(f'Correspondig error: {str(e)}')
                     await asyncio.sleep(5)
             elif packet_type == HANDSHAKE_OK:
-                logging.info(f'Hands shaked with hub')
+                logger.info(f'Hands shaked with hub')
+                self._last_heartbeat_received = datetime.datetime.now()
+                self._handsshaked = True
             elif packet_type == HANDSHAKE_FAIL:
-                logging.warning(f'Hub does not want to shake hands')
+                logger.error(f'Hub does not want to shake hands')
             elif packet_type == HEARTBEAT:
-                logging.debug(f'Heartbeat back from hub')
+                logger.debug(f'Heartbeat back from hub')
+                self._last_heartbeat_received = datetime.datetime.now()
             elif packet_type == RESPONSE_OK:
-                logging.debug(f'Hub received update correctly')
+                logger.debug(f'Hub received update correctly')
             elif packet_type == UNKNOWN_CLIENT:
-                logging.error(f'Hub does not recognize us')
+                logger.error(f'Hub does not recognize us')
                 await self._handshake()
             else:
                 if packet_type in self._cbs.keys():
                     await self._cbs.get(packet_type)(data)
                 else:
-                    logging.error(
+                    logger.error(
                         f'Message type not implemented: {packet_type}')
             await asyncio.sleep(1)
 
     async def _send_message(self, length, message_type, data):
         header = create_header(length, message_type)
 
-        logging.debug(f"Sending type: {message_type}")
+        logger.debug(f"Sending type: {message_type}")
         self._writer.write(header + data)
         try:
             await self._writer.drain()
@@ -170,7 +214,7 @@ class Client:
             'client_id': str(self._id),
             'client_type': self._client_type,
             'token': self._token,
-            'version': self._client_version}
+            'lib_version': self._client_version}
         if self._handshake_data_cb is not None:
             handshake_data = await self._handshake_data_cb()
             data = {**data, **handshake_data}
@@ -179,7 +223,7 @@ class Client:
         self._last_heartbeat_send = datetime.datetime.now()
 
     async def _send_heartbeat(self):
-        logging.debug('Sending heartbeat to hub')
+        logger.debug('Sending heartbeat to hub')
         id_encoded = qpack.packb(self._id)
         await self._send_message(len(id_encoded), HEARTBEAT, id_encoded)
         self._last_heartbeat_send = datetime.datetime.now()
